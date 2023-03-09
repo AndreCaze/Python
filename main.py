892b33d246879fce22fd9591b47775bc93396a77
Miguel Argote Reyes <miargote@hotmail.com>
2023-03-09 15:19:37 -0800

    Modificacion a pagina principal

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "<center><h1>HOLA ITE Ensenada</h1></center>"
+	return "<center><h1>LAPSHOP PRIME</h1></center>"
 
 @app.route("/laptops")
 def laptops():
-	return "<h1>Pagina Principal del Catalogo</h1>"
+	return "<h1>Catalogo de laptops</h1>"
 
 @app.route("/about")
 def about():
@@ -18,7 +18,7 @@ def about():
 	return datos
 
 @app.route("/about/terms-of-use")
-def usos():
+def terms():
 	return "<h1>Pagina en Mantenimiento</h1>"
 
 if __name__=='__main__':
