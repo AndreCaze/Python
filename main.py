0d5edd19e4059560431e39f0a2f4c7547b15a8f6
Andre Cadena <al19760606@ite.edu.mx>
2023-03-10 16:20:19 -0800

    Se agrego la consulta a tabla SQL

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+#from urllib.com import urlopen
+import mysql.connector
 import json
 
 app = Flask(__name__)
@@ -11,17 +14,35 @@ def main():
 def laptops():
 	return "<h1>Catalogo de laptops</h1>"
 
+@app.route("/about/terms-of-use")
+def terms():
+	return "<h1>Pagina en Mantenimiento</h1>"
+
 @app.route("/about")
 def about():
 	with open("about.json") as archivo:
 		datos = json.load(archivo)
 	return datos
 
-@app.route("/about/terms-of-use")
-def terms():
-	return "<h1>Pagina en Mantenimiento</h1>"
+@app.route("/example")
+def example():
+	return jsonify(saludo="bye bye")
+
+@app.route("/api/sql/test")
+def sqltest():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="Laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM Laptop")
+
+	resultados = cursor.fetchall()
+	return resultados
+
 
 if __name__=='__main__':
 	app.run()
-
-
