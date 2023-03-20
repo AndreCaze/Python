8ac744f135d3751c3793694ff1f062e25b389ff9
Andre Cadena <al19760606@ite.edu.mx>
2023-03-20 12:57:09 -0700

    Se añadieron las consultas a las demas tablas

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 def main():
 	return "<center><h1>LAPSHOP PRIME</h1></center>"
 
+
 @app.route("/laptops")
 def laptops():
 	return "<h1>Catalogo de laptops</h1>"
+
 
 @app.route("/about/terms-of-use")
 def terms():
@@ -24,21 +26,19 @@ def about():
 		datos = json.load(archivo)
 	return datos
 
-@app.route("/example")
-def example():
-	return jsonify(saludo="bye bye")
 
-@app.route("/api/sql/test")
-def sqltest():
+#Consultar Todos Laptops
+@app.route("/laptops/all")
+def example():
 	conexion = mysql.connector.connect(
     	host="127.0.0.1",
     	user="root",
     	password="Charles120Ahoy",
-    	database="Laptops"
+    	database="laptops"
 	)
 
 	cursor = conexion.cursor()
-	cursor.execute("SELECT * FROM Laptop")
+	cursor.execute("SELECT * FROM laptops")
 	headers=[x[0] for x in cursor.description]
 	resultados = cursor.fetchall()
 	json_data=[]
@@ -46,5 +46,112 @@ def sqltest():
 		json_data.append(dict(zip(headers,result)))
 	return jsonify(json_data)
 
+
+#Consultar Todos Fabricantes
+@app.route("/manufacturer/all")
+def manufacturer():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM manufacturer")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+#Consultar Todos Procesadores
+@app.route("/processor/all")
+def processor():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM processor")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+#Consultar Todos Tarjetas Graficas
+@app.route("/graphics/all")
+def graphics():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM graphics_card")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+#Consultar Todos Series
+@app.route("/series/all")
+def series():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM series")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+
+
+#Prueba
+@app.route("/api/sql/test")
+def sqltest():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM laptops")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+
+
+
 if __name__=='__main__':
 	app.run()
+
