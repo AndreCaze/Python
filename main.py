7a4c2467ad39bb5b1014439f9810caaee0053140
Andre Cadena <al19760606@ite.edu.mx>
2023-03-24 12:46:30 -0700

    Se agregaron mas endpoints

--- main.py
+++ main.py
@@ -127,6 +127,68 @@ def series():
 	return jsonify(json_data)
 
 
+#Consultar Todos RAM_Sticks
+@app.route("/ram/all")
+def ram():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM ram_sticks")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+#Consultar Todos Screens
+@app.route("/screens/all")
+def screens():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM screens")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+#Consultar Todos Storage_drives
+@app.route("/storages/all")
+def storage():
+	conexion = mysql.connector.connect(
+    	host="127.0.0.1",
+    	user="root",
+    	password="Charles120Ahoy",
+    	database="laptops"
+	)
+
+	cursor = conexion.cursor()
+	cursor.execute("SELECT * FROM storage_drives")
+	headers=[x[0] for x in cursor.description]
+	resultados = cursor.fetchall()
+	json_data=[]
+	for result in resultados:
+		json_data.append(dict(zip(headers,result)))
+	return jsonify(json_data)
+
+
+
+
 
 
 #Prueba
